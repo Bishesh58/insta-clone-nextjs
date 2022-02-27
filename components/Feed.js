9cff183fddbe54b56597ecbd8db066fd93d62a1b
Bishesh <bishesh.sunam@gmail.com>
2022-02-27 21:29:53 +1300

    added sign in and signout profile  with google account

--- components/Feed.js
+++ components/Feed.js
@@ -2,20 +2,25 @@ import Stories from '../components/Stories'
 import Posts from '../components/Posts'
 import MiniProfile from '../components/MiniProfile'
 import Suggestions from '../components/Suggestions'
+import { useSession } from 'next-auth/react'
 
 function Feed() {
+  const { data: session } = useSession()
   return (
-    <main className="mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3">
+    <main className={`mx-auto grid grid-cols-1 md:max-w-3xl md:grid-cols-2 xl:max-w-6xl xl:grid-cols-3
+     ${!session && "!grid-cols-1 !max-w-3xl"}`}>
       <section className="col-span-2">
         <Stories />
         <Posts />
       </section>
-      <section className="hidden md:col-span-1 xl:inline-grid">
-        <div className="fixed top-20">
-          <MiniProfile />
-          <Suggestions />
-        </div>
-      </section>
+      {session && (
+        <section className="hidden md:col-span-1 xl:inline-grid">
+          <div className="fixed top-20">
+            <MiniProfile />
+            <Suggestions />
+          </div>
+        </section>
+      )}
     </main>
   )
 }
--- components/MiniProfile.js
+++ components/MiniProfile.js
@@ -1,16 +1,23 @@
+import {signOut, useSession} from "next-auth/react"
+
 function MiniProfile() {
+  const {data: session} = useSession();
+  console.log(session);
   return (
     <div className="mt-14 ml-10 flex items-center justify-between ">
       <img
         className="h-16 w-16 rounded-full border object-contain p-[2px] "
-        src="https://thumbs.dreamstime.com/b/handsome-man-black-suit-white-shirt-posing-studio-attractive-guy-fashion-hairstyle-confident-man-short-beard-125019349.jpg"
+        src={session?.user?.image}
         alt=""
       />
       <div className='flex-1 mx-4'>
-        <h2 className='font-bold'>bisheshss</h2>
+        <h2 className='font-bold'>{session?.user?.username}</h2>
         <h3 className='text-sm text-gray-400'>Welcome to Insta-clone</h3>
       </div>
-      <button className='text-blue-400 text-sm font-semibold'>Sign out</button>
+      <button className='text-blue-400 text-sm font-semibold'
+      onClick={signOut}
+      
+      >Sign out</button>
     </div>
   )
 }
